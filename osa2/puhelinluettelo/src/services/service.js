5c2090be8cd4a54314c98a568003a6bcd634334d
jiiaa <jukkaa@live.com>
2020-04-10 17:03:55 +0300

    Tehtätäv 3.18 valmis.

--- osa2/puhelinluettelo/src/services/service.js
+++ osa2/puhelinluettelo/src/services/service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons';
+// const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = '/api/persons';
+// const baseUrl = 'https://mooc-osa3.herokuapp.com/api/persons';
 
 const getAll = () => {
     const request = axios.get(baseUrl);
