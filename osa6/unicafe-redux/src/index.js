a75550181c2223f7a0edbe3ea09a18d12f9139f1
jiiaa <jukkaa@live.com>
2020-07-25 12:38:12 +0300

    Osa 6 aloitettu. Tehtävät 6.1 ja 6.2 valmiina.

---
+++ osa6/unicafe-redux/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const neutral = () => {
+    store.dispatch({
+      type: 'OK'
+    });
+  };
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    });
+  };
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={good}>Good</button> 
+      <button onClick={neutral}>Neutral</button> 
+      <button onClick={bad}>Bad</button>
+      <div>
+        <button onClick={zero}>Reset stats</button>
+      </div>
+      <div>Good: {store.getState().good}</div>
+      <div>Neutral: {store.getState().ok}</div>
+      <div>Bad: {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
