ff428ae0aae9bab2985d7813a7df20ceb7fc524b
jiiaa <jukkaa@live.com>
2021-03-21 09:55:38 +0200

    Tehtävä 8.25 valmis.

--- osa8/kirjasto-front/src/App.js
+++ osa8/kirjasto-front/src/App.js
@@ -11,7 +11,7 @@ import Books from './components/Books';
 import LoginForm from './components/LoginForm';
 import NewBook from './components/NewBook';
 import Recommend from './components/Recommend';
-import { BOOK_ADDED } from './queries';
+import { ALL_BOOKS, BOOK_ADDED } from './queries';
 
 const Notification = ({ message }) => {
   if (!message) return null;
@@ -29,10 +29,23 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const client = useApolloClient();
 
+  const updateCacheWith = (addedBook) => {
+    const includedIn = (set, object) =>
+      set.map(book => book.id).includes(object.id)
+
+    const dataInStore = client.readQuery({ query: ALL_BOOKS });
+    if (!includedIn(dataInStore.allBooks, addedBook)) {
+      client.writeQuery({
+        query: ALL_BOOKS,
+        data: { allBooks : dataInStore.allBooks.concat(addedBook) }
+      });
+    }
+  };
+
   useSubscription(BOOK_ADDED, {
     onSubscriptionData: ({ subscriptionData }) => {
-      const data = subscriptionData.data.bookAdded;
-      window.alert(`New book with title ${data.title} was added. Written by ${data.author.name}`);
+      const addedBook = subscriptionData.data.bookAdded;
+      updateCacheWith(addedBook)
     }
   });
 
